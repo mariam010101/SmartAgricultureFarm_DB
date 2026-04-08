b2259652c8ce4cd3b0b9b63a7d531dfb62978769
Mariam Ghevondyan <i23.ghevondyan.mariam@etud.ufar.am>
2026-04-09 00:41:51 +0400

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-DB_CONFIG = {
-    "driver": "ODBC Driver 18 for SQL Server",
-    "server": "localhost,1433",
-    "database": "SmartAgricultureDB",
-    "username": "sa",
-    "password": "Str0ngP@ssw0rd!"
-}
