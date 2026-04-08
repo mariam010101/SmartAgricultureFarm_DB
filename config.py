dde2ebd06f2b6cf190af1d58047f452ec3c789c6
Mariam Ghevondyan <i23.ghevondyan.mariam@etud.ufar.am>
2026-04-09 00:28:28 +0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+DB_CONFIG = {
+    "driver": "ODBC Driver 18 for SQL Server",
+    "server": "localhost,1433",
+    "database": "SmartAgricultureDB",
+    "username": "sa",
+    "password": "Str0ngP@ssw0rd!"
+}
